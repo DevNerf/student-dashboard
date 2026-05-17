92b3cec641b2de18e876a8598c0aad89a9f37c4c
range4 <nuzhin100vlad@gmail.com>
2026-05-17 20:22:01 +0300

    first commit

---
+++ src/main/java/com/studentportal/studentcabinet/StudentCabinetApplication.java
@@ -0,0 +1,13 @@
+package com.studentportal.studentcabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCabinetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCabinetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studentportal/studentcabinet/StudentCabinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentportal.studentcabinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCabinetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
